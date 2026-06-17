ae48ef39721e374e0f53c2a5ec2231854870c1ff
Alan Do <daidx97@gmail.com>
2026-06-17 23:12:33 +0700

    Migrate to Nextra v2 and Next.js v14

---
+++ theme.config.jsx
@@ -0,0 +1,20 @@
+export default {
+  logo: <strong>FRT FinOps</strong>,
+  project: {
+    link: 'https://github.com/alan-do/frt-finops-gitbook'
+  },
+  i18n: [
+    { locale: 'vi', text: 'Tiếng Việt' },
+    { locale: 'en', text: 'English' }
+  ],
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – FRT FinOps'
+    }
+  },
+  primaryHue: 20, // Orange
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
+  }
+}
